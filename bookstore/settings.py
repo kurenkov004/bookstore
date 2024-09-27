65a78cb09c8188322140a9fc803ad086c4f03934
kurenkov004 <mary.kurenkov@gmail.com>
2024-09-27 14:31:55 -0400

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -20,11 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_9t0zf6rw=2hh=cqy!s!0ce0q(l0@5lw&pz-2m#%1dv0*-v+21'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
